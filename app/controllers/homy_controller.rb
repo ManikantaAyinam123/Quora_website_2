46070146dbfc84163b4ba64bfdac591bd5b90602
Manikantaayinam <manikanta@actimize.in>
2024-02-15 18:39:51 +0530

    Questions page on 15/02/2024

--- app/controllers/homy_controller.rb
+++ app/controllers/homy_controller.rb
@@ -1,10 +1,13 @@
 class HomyController < ApplicationController
   before_action :authenticate_user!
   def index
-    if user_signed_in? && current_user.email_verified === true 
-     
+    if user_signed_in?
+   
       redirect_to home_index_path
   
+    elsif user_signed_in? && current_user.email_verified === true 
+      
+      redirect_to home_index_path
     else
      
       redirect_to otp_verifys_path
